cedfad1eedf3ea461393f02ff3f595a9b35df422
FranZk9 <64720043+FranZk9@users.noreply.github.com>
2022-09-11 22:33:41 -0300

    Update pila.py

--- pila.py
+++ pila.py
@@ -29,63 +29,93 @@ pilita=pila()
 elementos=0
 largos=[]
 while True:
-    print("- Elija una operación -")    
-    opcion=int(input("1 - almacenar texto en pila\n2 - ver texto más largo y más corto\n3 - imprimir un texto de la pila\n4 - comparar tamaños\n5 - borrar pila\n6 - salir\n"))
-    if opcion==1:
-        texto=str(input("texto a apilar: "))
-        if texto == '':
-            print("Entrada vacía")
-        else:
-            pilita.apilar(texto)
-            elementos+=1
-            largos.append([pilita.obtenerLargo(),elementos])
-            pilita.ver()
-        #print(largos)
-    elif opcion==2:
-        masLargo=max(largos)
-        masCorto=min(largos)
-        #print(masLargo)
-        queElementoL=masLargo[1]-1
-        queElementoC=masCorto[1]-1
-        print("El texto más largo es:",pilita.verTexto(queElementoL))    
-        print("El texto más corto es: ",pilita.verTexto(queElementoC))
-    elif opcion==3:
-        textoImprimir=int(input("número del elemento que desea ver: "))-1
-        print(pilita.verTexto(textoImprimir))
-    elif opcion==4:
-        primer=int(input("# Primer elemento a comparar: "))
-        seg=int(input("# Segundo elemento a comparar: "))
-        pos1=0
-        pos2=0
-        #print(len(largos))
-        if primer>len(largos) or seg>len(largos):
-            print("Esta posición no existe")
-            logging.info('Posición {} no existe'.format(primer))
-        else:
-            for largo in largos:
-                if primer in largo:
-                    primer=largo[0]
-                    pos1=largo[1]-1
-            for largo in largos:
-                if seg in largo:
-                    seg=largo[0]
-                    pos2=largo[1]-1
-                #else:
-                #    print("Esta posición no existe")
-                #    logging.info('Posición {} no existe'.format(primer))
-            if primer<seg:
-                print(pilita.verTexto(pos2),"es más largo que",pilita.verTexto(pos1))
-                logging.info('texto de {} es más largo que el de {}'.format(pos2+1,pos1+1))
-            elif primer>seg:
-                print(pilita.verTexto(pos1),"es más largo que",pilita.verTexto(pos2))
-                logging.info('texto de {} es más largo que el de {}'.format(pos1+1,pos2+1))
+    print("------------------------")
+    print("- Elija una operación -")
+    try:    
+        opcion=int(input("1 - almacenar texto en pila\n2 - ver texto más largo y más corto\n3 - imprimir un texto de la pila\n4 - comparar tamaños\n5 - desapilar texto\n6 - salir\n"))
+        if opcion==1:
+            texto=str(input("texto a apilar: "))
+            if texto == '':
+                print("Entrada vacía")
             else:
-                print("Ambos textos miden lo mismo")
-    elif opcion==5:
-        pilita.desapilar()
-    elif opcion==6:
-        logging.info('Terminar')
-        break
-    else:
-        print("Opción no válida")
-        logging.info('Operacion inválida')
+                pilita.apilar(texto)
+                elementos+=1
+                largos.append([pilita.obtenerLargo(),elementos])
+                pilita.ver()
+            #print(largos)
+        elif opcion==2:
+            if pilita.is_empty()==True:
+                print("Pila vacía")
+            else:
+                masLargo=max(largos)
+                masCorto=min(largos)
+                #print(masLargo)
+                queElementoL=masLargo[1]-1
+                queElementoC=masCorto[1]-1
+                print("El texto más largo es:",pilita.verTexto(queElementoL))    
+                print("El texto más corto es: ",pilita.verTexto(queElementoC))
+        elif opcion==3:
+            if pilita.is_empty()==True:
+                print("Pila vacía")
+            else:
+                try:
+                    textoImprimir=int(input("número del elemento que desea ver: "))-1
+                    if (textoImprimir+1)>len(largos):
+                        print("No existe")
+                    else:
+                        print(pilita.verTexto(textoImprimir))
+                except:
+                    print("Formato inválido")
+        elif opcion==4:
+            if pilita.is_empty()==True:
+                print("Pila vacía")
+            else:
+                try:
+                    primer=int(input("# Primer elemento a comparar: "))
+                    seg=int(input("# Segundo elemento a comparar: "))
+                    pos1=0
+                    pos2=0
+                    #print(len(largos))
+                    if primer>len(largos):
+                        print("Esta posición no existe")
+                        logging.info('Posición {} no existe'.format(primer))
+                    elif seg>len(largos):
+                        print("Esta posición no existe")
+                        logging.info('Posición {} no existe'.format(seg))
+                    
+                    else:
+                        for largo in largos:
+                            if primer in largo:
+                                primer=largo[0]
+                                pos1=largo[1]-1
+                        for largo in largos:
+                            if seg in largo:
+                                seg=largo[0]
+                                pos2=largo[1]-1
+                            #else:
+                            #    print("Esta posición no existe")
+                            #    logging.info('Posición {} no existe'.format(primer))
+                        if primer<seg:
+                            print(pilita.verTexto(pos2),"es más largo que",pilita.verTexto(pos1))
+                            logging.info('texto de {} es más largo que el de {}'.format(pos2+1,pos1+1))
+                        elif primer>seg:
+                            print(pilita.verTexto(pos1),"es más largo que",pilita.verTexto(pos2))
+                            logging.info('texto de {} es más largo que el de {}'.format(pos1+1,pos2+1))
+                        else:
+                            print("Ambos textos miden lo mismo")
+                except:
+                    print("Formato inválido")
+        elif opcion==5:
+            pilita.desapilar()
+            if pilita.is_empty()==True:
+                print("Pila ya está vacía")
+            else:
+                pilita.ver()
+        elif opcion==6:
+            logging.info('Terminar')
+            break
+        else:
+            print("Opción no válida")
+            logging.info('Operacion inválida')
+    except:
+        print("Formato inválido")
